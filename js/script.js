dc71c8dff4dc794e8ac57d83226a8a5dab5c11ba
Radoslaw Zaborski <radex171@gmail.com>
2020-03-02 12:13:51 +0100

    need correct sidebar for mobile devices

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function clickHamburgerButton(){
   const button = document.querySelector('.hamb-menu');
-  
+  const topBar = document.querySelector('.top-bar');
+  const hambMenu = document.querySelector('.burger-menu .button');
+  const textMenu = document.querySelector('.burger-menu .text');
+  const underMenu = document.querySelector('.under-menu');
+  const sidebar = document.querySelector('.sidebar');
+  console.log(hambMenu);
+  console.log(topBar);
   button.addEventListener('click', function(){
     const thisButton = this;
     const button = thisButton.querySelector('.box-line');
@@ -9,6 +15,10 @@ function clickHamburgerButton(){
     console.log(button);
     button.classList.toggle('active');
     sidebarMenu.classList.toggle('hidden');
+    topBar.classList.toggle('close-menu');
+    underMenu.classList.toggle('close-menu');
+    textMenu.classList.toggle('close-menu');
+    sidebar.classList.toggle('width-menu');
   });
 }
 clickHamburgerButton();
