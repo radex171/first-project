0bd38dbb30f83a5fd202ab56d957565b6df458a3
Radoslaw Zaborski <radex171@gmail.com>
2020-03-02 21:13:20 +0100

    complete project for code review [v 1.2]

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function clickHamburgerButton(){
   const button = document.querySelector('.hamb-menu');
   const topBar = document.querySelector('.top-bar');
-  const hambMenu = document.querySelector('.burger-menu .button');
+  const hambMenu = document.querySelector('.container .container-sidebar .burger-menu .button');
   const textMenu = document.querySelector('.burger-menu .text');
   const underMenu = document.querySelector('.under-menu');
+  const containerSidebar = document.querySelector('.container-sidebar');
   const sidebar = document.querySelector('.sidebar');
+  const container = document.querySelector('.container');
   console.log(hambMenu);
   console.log(topBar);
   button.addEventListener('click', function(){
@@ -18,7 +20,10 @@ function clickHamburgerButton(){
     topBar.classList.toggle('close-menu');
     underMenu.classList.toggle('close-menu');
     textMenu.classList.toggle('close-menu');
-    sidebar.classList.toggle('width-menu');
+    sidebar.classList.toggle('close-menu');
+    containerSidebar.classList.toggle('close-menu');
+    hambMenu.classList.toggle('vis-hamb');
+    container.classList.toggle('content-max-width');
   });
 }
 clickHamburgerButton();
